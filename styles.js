2755068e4370e7fd855f4fb6bebc82fd1f4ae6b9
Renoj Varghese <renoj.varghese@uconn.edu>
2018-06-13 21:13:10 -0400

    updating styles

--- styles.js
+++ styles.js
@@ -1,7 +1,16 @@
 export default = {
     colors: {
-        blue: '#0032af',
-        red: '#89320e',
+        blue: '#437DFF',
+        blue10: "#6997ff",
+        blue20: "#b4cbff",
+        red: '#F24C55',
+        darkRed: '#b53940',
         yellow: "#fed03a",
+        white: "#ffffff",
+        grey: "#eeeeee",
+        grey10: "#f5f5f5"
+    },
+    effects: {
+        dropShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.2)"
     }
 }
