d78433caf78f421f8d4ea94ae4258c79a6b7a22f
Renoj Varghese <renoj.varghese@uconn.edu>
2018-06-25 22:33:55 -0400

    Starting main card display

---
+++ components/OfficialsDisplay/MainOfficial.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { StyleSheet, Text, View, Image } from "react-native";
+export class MainOfficial extends Component {}
+
+MainOfficial.propTypes = {
+    official: PropTypes.object.isRequired
+};
