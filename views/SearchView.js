8dc71b21d5017366568537d738b91c4d40b7514b
Renoj Varghese <renoj.varghese@uconn.edu>
2018-06-30 16:54:15 -0400

    converting to views to js files

---
+++ views/SearchView.js
@@ -0,0 +1,4 @@
+import SearchForm from "./components/SearchForm/SearchForm";
+import OfficialsContainer from "./components/OfficialsDisplay/OfficialsContainer";
+
+const store = createStore(officialReducer);
