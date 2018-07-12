bba8a1f0d0d40f50e068eceefffab9ca5f1afa9f
Renoj Varghese <renoj.varghese@uconn.edu>
2018-07-11 20:08:05 -0400

    updating reducers

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -1,18 +1,25 @@
 export const officialReducer = (
-    state = { officials: [], error: false },
+    state = { officials: [], error: false, mainOfficial: null },
     action
 ) => {
     switch (action.type) {
         case "NEW_STATE":
             return {
+                ...state,
                 officials: createOfficerArray(action.response),
                 error: false
             };
         case "ERROR":
             return {
+                ...state,
                 officials: [],
                 error: action.error
             };
+        case "MAIN":
+            return {
+                ...state,
+                mainOfficial: action.mainOfficial
+            }
         default:
             return state;
     }
