ed7d5a0bcf88a9b857c3d64404b7f247912ef13a
Renoj Varghese <renoj.varghese@uconn.edu>
2018-07-11 20:06:26 -0400

    MainOfficialView

---
+++ views/MainOfficialView.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View,Text } from "react-native";
+import { MainOfficial } from "../components/OfficialsDisplay/MainOfficial";
+export const MainOfficialView = () => {
+    return (
+        <View>
+            <MainOfficial/>
+        </View>
+    )
+}
