e23b0c8f582d5f44a939daa68e5e37d94d351da9
Renoj Varghese <renoj.varghese@uconn.edu>
2018-07-11 20:09:55 -0400

    connecting mainOfficial to redux store

--- components/OfficialsDisplay/MainOfficial.js
+++ components/OfficialsDisplay/MainOfficial.js
@@ -3,10 +3,18 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { StyleSheet, Text, View, Image } from "react-native";
 
-export const MainOfficial = () => {
+const MainOfficial = () => {
     return (
         <View>
             <Text>Main Official</Text>
         </View>
     )
 }
+
+const mapStateToProps({ state }) =>
+{
+    return {
+        mainOfficial: state.mainOfficial
+    }
+}
+export default connect(mapStateToProps)(mainOfficial)
